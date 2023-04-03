d4d56d73edc6c0b9c083a896a1e78177d6bf21dd
Dim0chka <Bugrick2004@yandex.ru>
2023-04-03 16:06:53 +0300

    new

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -2,7 +2,7 @@
     <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
         <div class="sm:flex sm:items-start">
           <div class="mt-3 text-center sm:mt-0 w-full">
-            <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">Создание поста</h3>
+            <h3 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl mb-8">Создание поста</h3>
             <div class="mt-2 w-full">
                 <my-input
                     class="mb-2 mt-2"
